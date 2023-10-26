aed35f9a9f292edbeccea95d001ff18812005eec
joshua nathaniel <ministerjoe456@gmail.com>
2023-10-27 00:23:48 +0100

    initialize form element and function handling error

---
+++ resources/scripts/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("registrationForm");
+    const email = document.getElementById("email");
+    const country = document.getElementById("country");
+    const zipCode = document.getElementById("zipCode");
+    const password = document.getElementById("password");
+    const passwordConfirmation = document.getElementById("passwordConfirmation");
+    const submitBtn = document.getElementById("submitBtn");
+    const successMessage = document.getElementById("successMessage");
+
+    function showError(field, errorDiv, message) {
+        field.classList.add("invalid");
+        errorDiv.textContent = message;
+    }
+})
+
